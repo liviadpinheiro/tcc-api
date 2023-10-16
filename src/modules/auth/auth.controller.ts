c8f7ac2540fb615cdfe4fe78b9cb9ce8e5aa3d34
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-15 22:32:21 -0300

    feat: return fullName at login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,12 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import { Controller, Post, Body } from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
-  @UseGuards(AuthGuard('local'))
   @Post('login')
-  async login(@Request() req: { user: { email: string; password: string } }) {
-    return this.authService.login(req.user);
+  async login(@Body() user: { email: string; password: string }) {
+    return this.authService.login(user);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -21,8 +21,12 @@ export class AuthService {
 
   async login(user: { email: string; password: string }) {
     const payload = { email: user.email, sub: user.password };
+
+    const userData = await this.userService.getUserByEmail(user.email);
+
     return {
       access_token: this.jwtService.sign(payload),
+      fullName: userData?.fullName,
     };
   }
 }
