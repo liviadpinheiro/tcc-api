0868a660a974848089d9dc16175fdfe5313c1e0e
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-17 16:19:30 -0300

    fix: cors error

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,19 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
+
   app.enableCors({
-    origin: process.env.ALLOWED_ORIGINS?.split(','),
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    allowedHeaders:
+      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
 
