58ebede8bbd2352036a7083f10ce5a6540e70165
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-19 12:40:12 -0300

    fix: contact module

--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
+import { ContactRepository } from './contact.repository';
+import { PrismaModule } from 'src/database/prisma.module';
 
 @Module({
   controllers: [ContactController],
-  providers: [ContactService],
+  providers: [ContactService, ContactRepository],
+  imports: [PrismaModule],
 })
 export class ContactModule {}
