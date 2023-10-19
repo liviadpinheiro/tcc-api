24c51ad2dc2e77537ebecbc539e027a36f2865dd
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-18 22:33:47 -0300

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -5,11 +5,9 @@ import * as cors from 'cors';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
-
   app.use(
     cors({
-      origin: allowedOrigins,
+      origin: process.env.FRONTEND_URL,
     }),
   );
 
