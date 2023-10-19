d5a3b31a2b22a5a2e760824b872a6be0ade717f0
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-18 22:13:39 -0300

    fix: update cors

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const allowedOrigins = process.env.ALLOWED_ORIGINS?.split(',');
 
-  app.enableCors({
-    origin: function (origin, callback) {
-      if (!origin || allowedOrigins?.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'));
-      }
-    },
-    allowedHeaders:
-      'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept, Observe',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    credentials: true,
-  });
+  app.use(
+    cors({
+      origin: allowedOrigins,
+    }),
+  );
 
   await app.listen(process.env.PORT || 3500);
 }
