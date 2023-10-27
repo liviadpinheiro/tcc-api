acdd109075d68e39d2f70347af2ef0c98f528f1f
Lívia Devolder Pinheiro <livia.pinheiro@clear.sale>
2023-10-27 03:10:15 -0300

    feat: render image at pdf

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cors from 'cors';
+import { json, urlencoded } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +11,9 @@ async function bootstrap() {
       origin: process.env.FRONTEND_URL,
     }),
   );
+
+  app.use(json({ limit: '10mb' }));
+  app.use(urlencoded({ limit: '10mb', extended: true }));
 
   await app.listen(process.env.PORT || 3500);
 }
